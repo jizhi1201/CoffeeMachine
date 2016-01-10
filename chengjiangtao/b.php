3623ddb5a1eda56e28865d173c6a4c90525af557
chengjiangtao <1271419861@qq.com>
2016-01-10 09:18:12 +0800

    计算总价

---
+++ chengjiangtao/b.php
@@ -0,0 +1,12 @@
+<?php
+$kouwei=$_GET["select"];
+$daxiao=$_GET["RadioGroup1"];
+$peiliao=$_GET["RadioGroup2"];
+$wendu=$_GET["RadioGroup3"];
+echo $kouwei;
+echo"<br/>";
+echo"总价钱";
+echo $kouwei+$daxiao;
+
+
+?>
